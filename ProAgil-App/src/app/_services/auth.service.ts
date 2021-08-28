dc4381d44ebb7cb6ea856b6aa19415296e699847
igormcouto <52253601+igormcouto@users.noreply.github.com>
2021-08-28 10:12:55 -0300

    Subindo service auth user

--- ProAgil-App/src/app/_services/auth.service.ts
+++ ProAgil-App/src/app/_services/auth.service.ts
@@ -1,10 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-constructor() { }
+  baseURL = 'http://localhost:5000/api/user/';
+  jwtHelpter = new JwtHelperService();
+  decodeToken: any;
+
+  constructor(private http: HttpClient) { }
+
+  login(model: any){
+    return this.http
+           .post(`${this.baseURL}login`,model)
+           .pipe(
+             map((response: any) =>{
+                const user = response;
+                
+                if(user){
+                  localStorage.setItem('token', user.token);
+                  this.decodeToken = this.jwtHelpter.decodeToken(user.token);
+                }
+           }))
+  }
+
+  register(model: any){
+    return this.http.post(`${this.baseURL}register`,model)
+  }
+
+  loggedIn(){
+    const token = localStorage.getItem('token');
+    return !this.jwtHelpter.isTokenExpired(token);
+  }
 
 }
